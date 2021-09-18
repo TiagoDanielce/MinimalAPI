af03a02e12852bb128e48feb14c6fd27c805fb39
Tiago Danielce <tiagorodrigues_svp@hotmail.com>
2021-09-18 14:11:45 -0300

    Add swagger

--- src/Pokemon.Api/Program.cs
+++ src/Pokemon.Api/Program.cs
@@ -1,13 +1,21 @@
 using Pokemon.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//ConfigureServices
 builder.Services.AddSingleton<PokemonRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Configure
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapGet("/pokemons", (PokemonRepository repository) =>
 {
-    return repository.GetAll();
+    return Results.Ok(repository.GetAll());
 });
 
 app.MapGet("/pokemons/{id}", (PokemonRepository repository, int id) =>
